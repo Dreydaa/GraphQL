98b6ea17111cd6189f59f971d03f11e9cd501933
abutel <Bultel0alan@gmail.com>
2024-06-20 16:03:30 +0200

    r

--- index.js
+++ index.js
@@ -176,7 +176,7 @@ async function fetchSkillData(token) {
 
 
 function renderXPChart(transactions) {
-    console.log(transactions)
+    console.log(transactions);
     const filteredTransactions = transactions.filter(transaction => {
         return transaction.path.includes("/div-01") && !transaction.path.includes("piscine-js/");
     });
@@ -184,22 +184,15 @@ function renderXPChart(transactions) {
         return filteredTransaction.type === "xp";
     });
     const sortedData = data.slice().sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
-    // Create the SVG container
 
     const svgContainer = document.getElementById('xpChartContainer');
     const height = parseInt(svgContainer.style.height);
     const width = parseInt(svgContainer.style.width);
 
-
-
     const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     svg.setAttribute('width', '100%');
     svg.setAttribute('height', '100%');
 
-    
-
-
-    // Create the line
     const accumulatedValues = [];
     let accumulatedTotal = 0;
     sortedData.forEach((entry, index) => {
@@ -207,25 +200,28 @@ function renderXPChart(transactions) {
         accumulatedValues.push({ x: index, y: accumulatedTotal });
     });
 
-
-    // Calculate Y-axis step size
     const yAxisStep = Math.ceil(Math.max(accumulatedTotal));
 
-    // Calculate X-axis step size (assuming 30 days per month)
-    const dateStep = 30 * 24 * 60 * 60 * 1000; // 30 days in milliseconds
-    // Calculate the number of months between the start and end date
+    const dateStep = 30 * 24 * 60 * 60 * 1000;
     const startDate = new Date(sortedData[0].createdAt);
     const endDate = new Date(sortedData[sortedData.length - 1].createdAt);
-    const monthsDifference = (endDate.getFullYear() - startDate.getFullYear()) * 12 + endDate.getMonth() - startDate.getMonth() +1;
-    // Create the line
+    const monthsDifference = (endDate.getFullYear() - startDate.getFullYear()) * 12 + endDate.getMonth() - startDate.getMonth() + 1;
+
     const line = document.createElementNS('http://www.w3.org/2000/svg', 'polyline');
-    const points = accumulatedValues.map(entry => `${(entry.x / (sortedData.length - 1)) * width},${height - (entry.y / yAxisStep) * height}`);
-    line.setAttribute('points', points.join(' '));	
+    const points = accumulatedValues.map(entry => {
+        const x = (entry.x / (sortedData.length - 1)) * width;
+        const y = height - (entry.y / yAxisStep) * height;
+        if (isNaN(x) || isNaN(y)) {
+            console.error(`NaN detected in polyline points: x=${x}, y=${y}`);
+        }
+        return `${x},${y}`;
+    });
+    line.setAttribute('points', points.join(' '));
     line.setAttribute('fill', 'none');
     line.setAttribute('stroke', '#4267B2');
     line.setAttribute('stroke-width', '2');
     svg.appendChild(line);
-    // Create Y-axis ticks and labels
+
     for (let i = 0; i <= 10; i++) {
         const yAxisTick = document.createElementNS('http://www.w3.org/2000/svg', 'line');
         yAxisTick.setAttribute('x1', '0');
@@ -241,7 +237,7 @@ function renderXPChart(transactions) {
         yAxisLabel.textContent = Math.round(yAxisStep * (10 - i) / 10);
         svg.appendChild(yAxisLabel);
     }
-    // Create X-axis ticks and labels
+
     for (let i = 0; i <= monthsDifference; i++) {
         const dateForTick = new Date(startDate.getTime() + (i / monthsDifference) * monthsDifference * dateStep);
         const xAxisTick = document.createElementNS('http://www.w3.org/2000/svg', 'line');
@@ -262,20 +258,19 @@ function renderXPChart(transactions) {
     return accumulatedTotal;
 }
 
+
 function formatDate(date) {
     const options = { month: 'short', year: 'numeric' };
     return date.toLocaleDateString('en-US', options);
   }
 
 
-function renderSkillPieChart(skillLevels) {
-
-    console.log(skillLevels)
+  function renderSkillPieChart(skillLevels) {
+    console.log(skillLevels);
 
     const svglvlContainer = document.getElementById('skillsChartContainer');
     const height = parseInt(svglvlContainer.style.height);
     const width = parseInt(svglvlContainer.style.width);
-
 
     const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     svg.setAttribute('width', '100%');
@@ -283,33 +278,31 @@ function renderSkillPieChart(skillLevels) {
     const barWidth = width / Object.keys(skillLevels).length;
     let index = 0;
     for (const [skillType, level] of Object.entries(skillLevels)) {
-      const bar = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-      console.log(level.type, level.amount)
-      const barHeight = (level.amount / 100) * height;
-      const xPosition = index * barWidth;
-      const yPosition = height - barHeight;
-      bar.setAttribute('x', xPosition);
-      bar.setAttribute('y', yPosition);
-      bar.setAttribute('width', barWidth);
-      bar.setAttribute('height', barHeight);
-      bar.setAttribute('fill', '#4267B2');
-      svg.appendChild(bar);
-      // Display skill type labels
-      const label = document.createElementNS('http://www.w3.org/2000/svg', 'text');
-      label.setAttribute('x', xPosition + barWidth / 2);
-      label.setAttribute('y', height - 5);
-      label.setAttribute('fill', '#333');
-      label.setAttribute('text-anchor', 'middle');
-      label.textContent = level.type.substring(level.type.indexOf('_') + 1);
+        console.log(level.type, level.amount);
+        const barHeight = (level.amount / 100) * height;
+        const xPosition = index * barWidth;
+        const yPosition = height - barHeight;
+        if (isNaN(xPosition) || isNaN(yPosition) || isNaN(barHeight) || isNaN(barWidth)) {
+            console.error(`NaN detected in bar chart: x=${xPosition}, y=${yPosition}, width=${barWidth}, height=${barHeight}`);
+        }
+        const bar = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+        bar.setAttribute('x', xPosition);
+        bar.setAttribute('y', yPosition);
+        bar.setAttribute('width', barWidth);
+        bar.setAttribute('height', barHeight);
+        bar.setAttribute('fill', '#4267B2');
+        svg.appendChild(bar);
 
-      label.style.writingMode = 'vertical-lr';
-
-
-      label.style.fontSize = '14px';
-
-
-      svg.appendChild(label);
-      index++;
+        const label = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+        label.setAttribute('x', xPosition + barWidth / 2);
+        label.setAttribute('y', height - 5);
+        label.setAttribute('fill', '#333');
+        label.setAttribute('text-anchor', 'middle');
+        label.textContent = level.type.substring(level.type.indexOf('_') + 1);
+        label.style.writingMode = 'vertical-lr';
+        label.style.fontSize = '14px';
+        svg.appendChild(label);
+        index++;
     }
 
     for (let i = 0; i <= 11; i++) {
@@ -320,16 +313,17 @@ function renderSkillPieChart(skillLevels) {
         yAxisTick.setAttribute('y2', (i / 10) * height);
         yAxisTick.setAttribute('stroke', '#ccc');
         svg.appendChild(yAxisTick);
-      
+
         const yAxisLabel = document.createElementNS('http://www.w3.org/2000/svg', 'text');
         yAxisLabel.setAttribute('x', '5');
         yAxisLabel.setAttribute('y', height - (i / 10) * height - 5);
         yAxisLabel.setAttribute('fill', '#333');
         yAxisLabel.textContent = Math.round(100 * i / 10); // Adjusted here
         svg.appendChild(yAxisLabel);
-      }
-      svglvlContainer.appendChild(svg);
+    }
+    svglvlContainer.appendChild(svg);
 }
+
 
 
 function logoutUser() {
