056ee0095479a1944c053a4ddc9b9bde0268b1a6
abutel <Bultel0alan@gmail.com>
2024-06-20 12:49:25 +0200

    3

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const loginSection = document.getElementById("loginSection");
-const profileSection = document.getElementById("profileSection");
-const userInput = document.getElementById("userInput");
-const passInput = document.getElementById("passInput");
-const signInBtn = document.getElementById("signInBtn");
-const loginError = document.getElementById("loginError");
-const displayUsername = document.getElementById("displayUsername");
-const userDataContainer = document.getElementById("userData");
-const signOutBtn = document.getElementById("signOutBtn");
+console.log("ici");
+
+const loginSection = document.querySelectorAll(".loginSection");
+const profileSection = document.getElementById("profileSection")[0];
+const userInput = document.getElementById("userInput")[0];
+const passInput = document.getElementById("passInput")[0];
+const signInBtn = document.querySelectorAll(".signInBtn");
+const loginError = document.getElementById("loginError")[0];
+const displayUsername = document.getElementById("displayUsername")[0];
+const userDataContainer = document.getElementById("userData")[0];
+const signOutBtn = document.querySelectorAll("signOutBtn");
 
 let jwtToken = null;
 
 async function authentificateUser() {
+    console.log("rpes");
     const username = document.getElementById('userInput').value;
     const password = document.getElementById('password').value;
     const base64Credentials = btoa(`${username}:${password}`);
